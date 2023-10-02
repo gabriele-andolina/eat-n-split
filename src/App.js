42aa0bce34055a763fe5fbe4efe4a744fbd79fa5
gabriele-andolina <gabe.andolina@gmail.com>
2023-10-02 21:07:49 +0200

    State: Created name and image state variables and functions in AddFriendForm

--- src/App.js
+++ src/App.js
@@ -86,13 +86,44 @@ function Friend({ friend }) {
 }
 
 function AddFriendForm() {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("https://i.pravatar.cc/48?u=499476");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!name || !image) return;
+
+        const id = crypto.randomUUID();
+
+        const newFriend = {
+            id,
+            name,
+            image: `${image}?=${id}`,
+            balance: 0,
+        };
+
+        console.log(newFriend);
+
+        setName("");
+        setImage("https://i.pravatar.cc/48");
+    }
+
     return (
-        <form className="add-friend-form">
+        <form className="add-friend-form" onSubmit={handleSubmit}>
             <label>Friend name</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
 
             <label>Image URL</label>
-            <input type="text" />
+            <input
+                type="text"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+            />
 
             <Button>Add</Button>
         </form>
